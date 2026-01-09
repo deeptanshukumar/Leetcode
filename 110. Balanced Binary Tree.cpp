33d896b8fd8072474889a553224f98091ad31bcd
Deeptanshu Kumar <70989817+deeptanshukumar@users.noreply.github.com>
2026-01-09 22:59:31 +0530

    Add files via upload

---
+++ 110. Balanced Binary Tree.cpp
@@ -0,0 +1,74 @@
+/*
+110. Balanced Binary Tree
+
+Given a binary tree, determine if it is
+
+.
+
+ 
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+
+Example 2:
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+
+Example 3:
+
+Input: root = []
+Output: true
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [0, 5000].
+    -104 <= Node.val <= 104
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <cmath>
+using namespace std;
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        int left = height(root->left);
+        if (left == -1) return -1;
+
+        int right = height(root->right);
+        if (right == -1) return -1;
+
+        if (abs(left - right) > 1) return -1;
+
+        return max(left, right) + 1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return height(root) != -1;
+    }
+};
