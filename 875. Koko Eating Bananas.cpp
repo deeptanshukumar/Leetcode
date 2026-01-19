a2e7b02b58dcde51aaac92e62294c696cfd25290
Deeptanshu Kumar <70989817+deeptanshukumar@users.noreply.github.com>
2026-01-19 18:12:37 +0530

    Add files via upload

---
+++ 875. Koko Eating Bananas.cpp
@@ -0,0 +1,62 @@
+/*
+875. Koko Eating Bananas
+
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+
+Example 2:
+
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+
+Constraints:
+
+    1 <= piles.length <= 104
+    piles.length <= h <= 109
+    1 <= piles[i] <= 109
+*/
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    long long calculatetime(vector<int>& piles, int speed){
+        long long time = 0;
+        for(auto i:piles){
+            time+=(i+speed-1)/speed;
+        }
+        return time;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        sort(piles.begin(), piles.end());
+        int low = 1;
+        int high = piles[piles.size()-1];
+        while(low<=high){
+            long long mid = low + (high-low)/2;
+            if(calculatetime(piles, mid)<=h){
+                high=mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
