9b6673bf42ced8f78bc0de846c5f011c8eef065a
Deeptanshu Kumar <70989817+deeptanshukumar@users.noreply.github.com>
2026-01-28 07:57:36 +0530

    Add files via upload

---
+++ 101. Symmetric Tree.cpp
@@ -0,0 +1,63 @@
+/*
+101. Symmetric Tree
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+ 
+
+Example 1:
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 1000].
+    -100 <= Node.val <= 100
+
+ 
+Follow up: Could you solve it both recursively and iteratively?
+ */
+
+
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    bool ismirror(TreeNode* a, TreeNode* b){
+        if(!a&&!b) return true;
+        if(!a||!b) return false;
+
+        return (a->val==b->val)&&ismirror(a->left, b->right)&&ismirror(a->right, b->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        return ismirror(root->left, root->right);
+    }
+};
