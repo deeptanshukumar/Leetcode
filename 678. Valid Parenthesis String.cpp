47ce58bd8cedaaf0f45b58a1ff135f5190760388
Deeptanshu Kumar <70989817+deeptanshukumar@users.noreply.github.com>
2026-02-23 01:05:00 +0530

    Add files via upload

---
+++ 678. Valid Parenthesis String.cpp
@@ -0,0 +1,60 @@
+/*
+678. Valid Parenthesis String
+
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+    Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+    Any right parenthesis ')' must have a corresponding left parenthesis '('.
+    Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+    '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "(*)"
+Output: true
+
+Example 3:
+
+Input: s = "(*))"
+Output: true
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 100
+    s[i] is '(', ')' or '*'.
+
+*/
+
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int min = 0, max = 0;
+        for(auto i:s){
+            if(i=='('){
+                min++, max++;
+            }
+            else if(i==')'){
+                min--, max--;
+            }
+            else if(i=='*'){
+                min--;max++;
+            }
+            if(max<0) return false;
+            if(min<0) min=0;            
+        }
+        return min==0;
+    }
+};
