388899dc306c3968afd34f612765a71223647c51
Deeptanshu Kumar <70989817+deeptanshukumar@users.noreply.github.com>
2026-03-04 00:08:07 +0530

    Add files via upload

---
+++ 1545. Find Kth Bit in Nth Binary String.cpp
@@ -0,0 +1,67 @@
+/*
+1545. Find Kth Bit in Nth Binary String
+
+Given two positive integers n and k, the binary string Sn is formed as follows:
+
+    S1 = "0"
+    Si = Si - 1 + "1" + reverse(invert(Si - 1)) for i > 1
+
+Where + denotes the concatenation operation, reverse(x) returns the reversed string x, and invert(x) inverts all the bits in x (0 changes to 1 and 1 changes to 0).
+
+For example, the first four strings in the above sequence are:
+
+    S1 = "0"
+    S2 = "011"
+    S3 = "0111001"
+    S4 = "011100110110001"
+
+Return the kth bit in Sn. It is guaranteed that k is valid for the given n.
+
+ 
+
+Example 1:
+
+Input: n = 3, k = 1
+Output: "0"
+Explanation: S3 is "0111001".
+The 1st bit is "0".
+
+Example 2:
+
+Input: n = 4, k = 11
+Output: "1"
+Explanation: S4 is "011100110110001".
+The 11th bit is "1".
+
+ 
+
+Constraints:
+
+    1 <= n <= 20
+    1 <= k <= 2n - 1
+*/
+
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    string findsthstring(string s){
+        string temp = "";
+        for(auto &i:s){
+            if(i=='1')temp+='0';
+            else temp+="1";
+        }
+        reverse(temp.begin(), temp.end());
+        s = s+"1"+temp;
+        return s;
+    }
+    char findKthBit(int n, int k) {
+        string s = "0";
+        for(int i=1;i<=n;i++){
+            s = findsthstring(s);
+        }
+        return s[k-1];
+    }
+};
